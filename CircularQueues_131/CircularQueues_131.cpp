c43785948bdec7a5d22eff1649bf78b4fc492436
ikhdaamel <ikhda.new45@gmail.com>
2023-06-22 15:05:42 +0700

    cek antrian kosong

--- CircularQueues_131/CircularQueues_131.cpp
+++ CircularQueues_131/CircularQueues_131.cpp
@@ -6,9 +6,9 @@ class Queues {
 	int queue_array[5]; 
 
 public:
-	Queues() {
-		FRONT = -1;
-		REAR = -1;
+	Queues() {								
+		FRONT = -1;										//set front = -1
+		REAR = -1;										//set rear = -1
 	}
 
 	void insert() {
@@ -23,5 +23,19 @@ public:
 			cout << "\nQueue overflow\n";
 			return;
 		}
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika benar berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
