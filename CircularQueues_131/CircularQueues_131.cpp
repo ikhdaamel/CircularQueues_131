0c44bde89ecab24890af4646db9df9c4e749ef30
ikhdaamel <ikhda.new45@gmail.com>
2023-06-22 16:00:48 +0700

    melakukan perubahan

--- CircularQueues_131/CircularQueues_131.cpp
+++ CircularQueues_131/CircularQueues_131.cpp
@@ -48,16 +48,16 @@ public:
 		cout << "\nthe element deleted from the queue is :" << queue_array[FRONT] << "\n";
 
 		//cek jika antrian hanya memiliki satu elemen
-		if (FRONT == REAR) {
-			FRONT = -1;
-			REAR = -1;
+		if (FRONT == REAR) {						//step 1
+			FRONT = -1;								//step 1.a
+			REAR = -1;								//step 1.b
 		}
 		else {
 			//jika elemen yg dihapus berada di posisi terakhir array, kembali ke awal array
-			if (FRONT == max - 1)				
-				FRONT = 0;
+			if (FRONT == max - 1)					//step 2	
+				FRONT = 0;							//step 2.a
 			else
-				FRONT = FRONT + 1;
+				FRONT = FRONT + 1;					//step 3
 		}
 	}
 	void display() {
